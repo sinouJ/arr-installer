bd51f49aacaacdc3cd8379e5807ae183e89b255f
Jordan Sinou <sinoujordan@gmail.com>
2024-01-14 00:11:27 +0100

    change path

--- download-clients-installer.sh
+++ download-clients-installer.sh
@@ -13,7 +13,7 @@ ___  ____ _  _ ____    _ _  _ ____ ___ ____ _    _    ____ ____
 #This script will install download clients in docker.
 
 ######################################################################
-docker_path=/var/lib/docker
+docker_path=/etc/docker    #Change this to the location of your docker-compose.yml file
 #Functions List
 
 noanswer () { echo "Skipping..." ; }
